0e214705dcc1a0499bb6ba97707e08a2c5cc48a6
RituSharma79 <61319337+RituSharma79@users.noreply.github.com>
2020-02-21 19:04:24 +0530

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+import "./src/assets/css/custom.css"
+import "./src/assets/css/responsive.css"
+import "./src/assets/css/font-awesome.min.css"
+import "./src/fonts//fonts.css"
